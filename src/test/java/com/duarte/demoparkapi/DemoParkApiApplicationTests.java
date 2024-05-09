f31b584b3ee2ae05fa9c8d1e25ae878b527581fc
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-05-09 13:04:22 -0300

    Projeto iniciado e configurado

---
+++ src/test/java/com/duarte/demoparkapi/DemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duarte.demoparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
