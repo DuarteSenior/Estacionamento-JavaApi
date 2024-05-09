b117a48892de7acd24b49f29b92b44c1a1fdf5ad
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-05-09 16:41:57 -0300

    Criação do repositório de usuarios

---
+++ src/main/java/com/duarte/demoparkapi/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.duarte.demoparkapi.repository;
+
+import com.duarte.demoparkapi.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
