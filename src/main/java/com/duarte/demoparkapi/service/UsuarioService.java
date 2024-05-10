117064611b0413657fe39703459290ee518688ad
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-05-09 21:50:13 -0300

    rota post de usuarios concluida

--- src/main/java/com/duarte/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/duarte/demoparkapi/service/UsuarioService.java
@@ -1,8 +1,10 @@
 package com.duarte.demoparkapi.service;
 
+import com.duarte.demoparkapi.entity.Usuario;
 import com.duarte.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -10,4 +12,9 @@ public class UsuarioService {
 
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return  usuarioRepository.save(usuario);
+    }
 }
